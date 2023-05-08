3c25021dd0521cc23b95cd9e6ff50018b0636d7b
xopherdeep <contact@midnightnerd.com>
2023-05-08 00:19:26 -0700

    ✨ Logout

--- src/components/Menu/MenuOptions/Logout.tsx
+++ src/components/Menu/MenuOptions/Logout.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LogoutIcon from '@icon/LogoutIcon';
 
 const Logout = () => {
+  const defaultApi = new URL(import.meta.env.VITE_DEFAULT_API_ENDPOINT)
+  const [isLoading, setIsLoading] = useState(false);
+  const handleOnClick = () => {
+    setIsLoading(true);
+    defaultApi.pathname = defaultApi.hostname.startsWith('api.')
+      ? '/logout'
+      : '/api/logout'
+    window.location.href = defaultApi.toString();
+  };
+
   return (
-    <a className='flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm'>
+    <a 
+      className='flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm'
+      onClick={handleOnClick}
+    >
       <LogoutIcon />
-      Log out
+      {isLoading ? 'Logging out...' : 'Log out'}
     </a>
   );
 };
